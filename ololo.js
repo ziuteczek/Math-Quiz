9be57b1007114013bfd3cbbbc5ee1f171b2571d4
Stanisław Dymny <dymnystan@gmail.com>
2023-10-28 12:26:40 +0200

    JS quiz generator added

---
+++ ololo.js
@@ -0,0 +1,49 @@
+'use strict'
+const generateQuizNumbers = function(quizLength,minNumber,maxNumber)
+{
+  const quizNumbers = [];
+  for (let i = 0; i < quizLength;i++)
+  {
+    const a = Math.round(Math.random() * (minNumber - maxNumber)) + maxNumber;
+    const b = Math.round(Math.random() * (minNumber - maxNumber)) + maxNumber;
+    quizNumbers.push([a,b]);
+  }
+  return quizNumbers;
+}
+const quizDiffHandler = function(diff,length)
+{
+  let quizNums;
+  switch(diff)
+  {
+    case 1:
+      quizNums = generateQuizNumbers(length,0,5);
+      break;
+    case 2:
+      quizNums = generateQuizNumbers(length,5,10);
+      break;
+    case 3:
+      quizNums = generateQuizNumbers(length,10,15);
+      break;
+    case 4:
+      quizNums = generateQuizNumbers(length,15,20);
+      break;
+    case 5:
+      quizNums = generateQuizNumbers(length,10,20);
+      break;
+  }
+  return quizNums;
+}
+
+
+const generateQuizQuestions = function(operation,difficulty,length)
+{
+  const quizNumbers = quizDiffHandler(difficulty,length);
+  const quizData = 
+  {
+    numbers: quizNumbers,
+    operation: operation,
+    answer: quizNumbers.map(nums => nums.reduce((a,b) => eval(`${a} ${operation} ${b}`)))
+  }
+  return quizData;
+}
+console.log(generateQuizQuestions('*',3,5));
