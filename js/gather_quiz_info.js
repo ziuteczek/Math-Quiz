411a8f037632d03516f1d47ece971b5fbc08824a
Stanisław Dymny <dymnystan@gmail.com>
2023-11-02 21:35:18 +0100

    Gather information function

--- js/gather_quiz_info.js
+++ js/gather_quiz_info.js
@@ -1,6 +1,36 @@
 'use strict';
-export function getQuizInfo(diffBtnsLocation){
-    const difficulties = [["łatwy","easy"],["średni","middle"],["trudny","hard"]];
-    difficulties.forEach((diff)=>diffBtnsLocation.insertAdjacentHTML("beforeend",`<button id="${diff[1]}" class="chose-diff-btn">${diff[0]}</button>`));
-    const choseBtns = diffBtnsLocation.querySelectorAll(".chose-diff-btn");
-};
+import { createQuiz } from './index.js';
+export function getQuizInfo(diffBtnsLocation, operation) {
+  const difficulties = [
+    ['łatwy', 'easy1'],
+    ['średni', 'middle2'],
+    ['trudny', 'hard3'],
+  ];
+  difficulties.forEach((diff) =>
+    diffBtnsLocation.insertAdjacentHTML(
+      'beforeend',
+      `<button id="${diff[1]}" class="chose-diff-btn">${diff[0]}</button>`
+    )
+  );
+  const choseBtns = diffBtnsLocation.querySelectorAll('.chose-diff-btn');
+  choseBtns.forEach((btn) =>
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      const quizDifficulty = Number(this.id.slice(-1));
+      const answerNegative = handleNegative(quizDifficulty,operation);
+      createQuiz(operation,quizDifficulty,quizDifficulty * 4,answerNegative);
+      choseBtns.forEach((btn) => btn.remove());
+    })
+  );
+}
+const handleNegative = function(difficulty,operation){
+    switch (true)
+    {
+        case difficulty>=2 && operation==="-":
+            return true;
+        case difficulty>=3 && (operation==="*" || operation==="/"):
+            return true; 
+        default:
+            return false
+    }
+}
--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ import * as gatherInfo from './gather_quiz_info.js';
 const pickQuizBtns = document.querySelectorAll('.chose-quiz__btn');
 pickQuizBtns.forEach(function (btn) {
   btn.addEventListener('click', function () {
-    gatherInfo.getQuizInfo(document.body);
+    gatherInfo.getQuizInfo(document.body,this.getAttribute("operation"));
   });
 });
+export function createQuiz(operator,difficulty,length,canBeNegative) {
+  // const quizInfo = generator.quizQuestionGenerator(operator,difficulty,length,canBeNegative);
+  console.log(operator,difficulty,length,canBeNegative);
+}
+function createQuizString() {
+  
+}
