182ae0a6fe85caf43d69e8e0b4432bd0cb251922
Stanisław Dymny <dymnystan@gmail.com>
2023-11-02 21:37:06 +0100

    little formating

--- js/quiz_question_generator.js
+++ js/quiz_question_generator.js
@@ -1,48 +1,59 @@
-'use strict'
-const handleNegative = (a,b) => a > b ? [a,b] : [b,a];
-const handlePositive = (a,b) => [a,b];
-const generateQuizNumbers = function(quizLength,minNumber,maxNumber,canBeNegative)
-{
+'use strict';
+const handleNegative = (a, b) => (a > b ? [a, b] : [b, a]);
+const handlePositive = (a, b) => [a, b];
+const generateQuizNumbers = function (
+  quizLength,
+  minNumber,
+  maxNumber,
+  canBeNegative
+) {
   const quizNumbers = [];
   const chceckfunction = canBeNegative ? handlePositive : handleNegative;
-  for (let i = 0; i < quizLength;i++)
-  {
+  for (let i = 0; i < quizLength; i++) {
     const a = Math.round(Math.random() * (minNumber - maxNumber)) + maxNumber;
     const b = Math.round(Math.random() * (minNumber - maxNumber)) + maxNumber;
-    quizNumbers.push(chceckfunction(a,b));
+    quizNumbers.push(chceckfunction(a, b));
   }
   return quizNumbers;
-}
-const quizDiffHandler = function(diff,length,canBeNegative)
-{
+};
+const quizDiffHandler = function (diff, length, canBeNegative) {
   let quizNums;
-  switch(diff)
-  {
+  switch (diff) {
     case 1:
-      quizNums = generateQuizNumbers(length,0,5,canBeNegative);
+      quizNums = generateQuizNumbers(length, 0, 5, canBeNegative);
       break;
     case 2:
-      quizNums = generateQuizNumbers(length,5,10,canBeNegative);
+      quizNums = generateQuizNumbers(length, 5, 10, canBeNegative);
       break;
     case 3:
-      quizNums = generateQuizNumbers(length,10,15,canBeNegative);
+      quizNums = generateQuizNumbers(length, 10, 15, canBeNegative);
       break;
     case 4:
-      quizNums = generateQuizNumbers(length,15,20,canBeNegative);
+      quizNums = generateQuizNumbers(length, 15, 20, canBeNegative);
       break;
     case 5:
-      quizNums = generateQuizNumbers(length,10,20,canBeNegative);
+      quizNums = generateQuizNumbers(length, 10, 20, canBeNegative);
       break;
   }
   return quizNums;
-}
-export function quizQuestionGenerator(operation,difficulty,length,canBeNegative){
-  const quizNumbers = quizDiffHandler(difficulty,length,canBeNegative=true);
-  const quizData = 
-  {
+};
+export function quizQuestionGenerator(
+  operation,
+  difficulty,
+  length,
+  canBeNegative
+) {
+  const quizNumbers = quizDiffHandler(
+    difficulty,
+    length,
+    (canBeNegative = true)
+  );
+  const quizData = {
     numbers: quizNumbers,
     operation: operation,
-    answer: quizNumbers.map(nums => nums.reduce((a,b) => eval(`${a} ${operation} ${b}`)))
-  }
-  return quizData; 
+    answer: quizNumbers.map((nums) =>
+      nums.reduce((a, b) => eval(`${a} ${operation} ${b}`))
+    ),
+  };
+  return quizData;
 }
