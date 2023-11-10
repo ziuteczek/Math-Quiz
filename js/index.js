12c1d02effd777e77caa8e2e8a200ef693706628
Stanisław Dymny <dymnystan@gmail.com>
2023-11-10 22:51:34 +0100

    page reload on logo click added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 import * as gatherInfo from './gather_quiz_info.js';
 const pickQuizBtns = document.querySelectorAll('.chose-quiz__btn');
 const btnsContainer = document.querySelector(".chose-quiz")
+document.querySelector(".nav-title").addEventListener("click",()=> location.reload())
 pickQuizBtns.forEach(function (btn) {
   btn.addEventListener('click', function () {
     pickQuizBtns.forEach(btn => btn.remove())
